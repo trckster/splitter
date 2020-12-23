6801b7a415106606ff9d66af4f269d3a6214cf76
Daniil <dan-trofimov@mail.ru>
2020-12-24 02:22:10 +0300

    Primitive bot

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/trckster/splitter
 
 go 1.15
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.0-rc1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-func main() {
-	var s = NewSpeaker("Some information")
+import (
+	"log"
+	"os"
 
-	s.Say()
+	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
